2f299e2b46fbf02ca717e72a324547c631c2ea8a
Jake McCrary <jake@jakemccrary.com>
2015-03-25 09:14:55 -0500

    adding org-present for jake.el

--- home/.emacs.d/user/jake.el
+++ home/.emacs.d/user/jake.el
@@ -60,3 +60,19 @@ This command does the reverse of `fill-paragraph'."
   (interactive)
   (let ((fill-column 90002000))
     (fill-paragraph nil)))
+
+(install-package 'org-present)
+(eval-after-load "org-present"
+  '(progn
+     (add-hook 'org-present-mode-hook
+               (lambda ()
+                 (org-present-big)
+                 (org-display-inline-images)
+                 (org-present-hide-cursor)
+                 (org-present-read-only)))
+     (add-hook 'org-present-mode-quit-hook
+               (lambda ()
+                 (org-present-small)
+                 (org-remove-inline-images)
+                 (org-present-show-cursor)
+                 (org-present-read-write)))))
