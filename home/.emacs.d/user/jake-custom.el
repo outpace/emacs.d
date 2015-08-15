aca3123e0c49a26f8b8d36c2784ba596ee70b7cc
Jake McCrary <jake@jakemccrary.com>
2015-08-15 10:26:28 -0500

    rainbow parens but only rainbow mismatched

--- home/.emacs.d/user/jake-custom.el
+++ home/.emacs.d/user/jake-custom.el
@@ -3,7 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(rainbow-delimiters-max-face-count 1))
+ '(rainbow-delimiters-max-face-count 1)
+ '(safe-local-variable-values
+   (quote
+    ((css-indent-offset . 2)
+     (whitespace-line-column . 80)
+     (lexical-binding . t)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
